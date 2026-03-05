db4457719a77693dac6bf43a5912eee323a09bf1
11249a050-pixel <11249a050@kanchiuniv.ac.in>
2026-03-05 01:38:31 -0800

    Add files via upload

---
+++ 11249A050-exp1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,a1=0,a2=1,next;
+    printf("enter the number of terms:");
+    scanf("%d",&n);
+    printf("Fibonacci series: %d %d",a1,a2);
+    for(int i=3;i<=n;i++){
+        next=a1+a2;
+        printf("%d",next);
+        a1=a2;
+        a2=next;
+    }
+    return 0;
+}
+
+
+
